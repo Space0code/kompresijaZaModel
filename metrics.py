28e3ce5c612105266f5ca76e34de4f356659f21f
mlfmf <136562674+mlfmf@users.noreply.github.com>
2023-08-04 15:08:16 +0200

    metrike osnovna koda

---
+++ metrics.py
@@ -0,0 +1,35 @@
+"""
+1. normaliziraj napovedi na [0, 1] (in ne [0, 255])
+2. izračunaj "vse" smiselne metrike:
+   - mse
+   - intersection over union
+   - (averege) precision
+   - (average) recall
+"""
+
+import numpy as np
+
+
+def intersection_over_union(img1, img2):
+    intersection = np.minimum(img1, img2)
+    union = np.maximum(img1, img2)
+    return np.sum(intersection) / np.sum(union)
+
+
+def precision(true, predicted, threshold):
+    img1_t = true > threshold
+    img2_t = predicted > threshold
+    intersection = np.minimum(img1_t, img2_t)
+    return np.sum(intersection) / np.sum(img1_t)
+
+
+def recall(true, predicted, threshold):
+    """
+    :param true: ground truth numpy array with values from [0, 1]
+    :param predicted: predicted numpy array with values from [0, 1]
+    :param threshold: threshold for binarization
+    """
+    true_t = true > threshold
+    pred_t = predicted > threshold
+    intersection = np.minimum(true_t, pred_t)
+    return np.sum(intersection) / np.sum(pred_t)
