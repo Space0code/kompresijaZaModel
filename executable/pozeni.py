d785fdd7e70ffc2f1940ee24e6017be16d1d3140
Space0code <bozak.tomi@gmail.com>
2023-08-04 19:45:20 +0200

    normaliziral mse

--- executable/pozeni.py
+++ executable/pozeni.py
@@ -1,30 +1,26 @@
 """
-1. Ideja poteka programa:
-- poženi model na originalnih slikah in shrani rezultate
-- stiskaj slike & sproti poganjaj model 
-    - original sliko stisni na Q kvalitete (oz. za X %, odvisno od programa)
-    - (shrani stisnjeno sliko?)
-    - poženi model nad sliko
-    - shrani rezultat v mapo (recimo v resultQ{Q}) 
-- primerjaj rezultate:
-    - ideja: število pikslov, ki se razlikujejo in za kakšen odstotek (odtenek) se razlikujejo
-"""
+Program se izvaja v štirih korakih:
+1) Model za prepoznavo razpok se požene na originalnih (OG) slikah.
+2) OG slike stisnemo
+    - za različne faktorje,
+    - z dvema različnima programoma za kompresijo: 
+        - eden (pcaColor) temelji na singularnem razcepu (SVD oz. PCA),
+        - drugi (pil) temelji na ??? TODO.
+3) Model za prepoznavo razpok poženemo nad kompresiranimi slikami. 
+4) Rezultate (slike), ki jih izpljune model primerjamo z različnimi metrikami:
+    - intersection over union,
+    - (average) precision,
+    - (average) recall,
+    - mean squared error (mse).
+    
+    Rezultate shranimo v csv datoteko. :)
 
 """
-2. Ideja poteka programa:
-1) poženi model na originalnih slikah in shrani rezultate
-2) for Q in (1, 5, 10, 15, ..., 90, 95):
-    - stisni in shrani slike na Q kvalitete
-3) for Q in (1, 5, 10, 15, ..., 90, 95):
-    - poženi model nad vsako sliko v mapi, kjer so shranjene slike kvalitete Q
-    - shrani rezultate
-4) primerjaj rezultate:
-    - ideja: število pikslov, ki se razlikujejo in za kakšen odstotek (odtenek) se razlikujejo
-"""
 
-#######
+
+#########
 # imports
-#######
+#########
 import os
 from pil_kompresor import compressImagePIL 
 from pcaColor_kompresor import compressImagePCA
@@ -60,17 +56,17 @@ R = []
 for i in range (12):
     R.append(2**i)
 
-
-#######
+"""
+##############################
 # 1) poženi model na OG slikah
-#######
+##############################
 
 out_pred_dir = os.path.join(args.out_pred_dir, 'OG')
 main(args.out_viz_dir, out_pred_dir, args.model_type, args.model_path, args.img_dir, args.subimage_size, args.threshold)
 
-#######
+###################################################
 # 2) stisni na q kvalitete oz. na r ranga in shrani
-#######
+###################################################
 
 # mapa, kjer so shranjene OG slike
 img_dir = args.img_dir #  "D:\\OneDrive\\Dokumenti_ne_sola\\Konferenca_STeKam\\2023\\git\\kompresijaZaModel\\test_images\\A_People\\images_first100\\images\\images"
@@ -102,9 +98,9 @@ for r in R:
         compressImagePCA(src_file, dest_file, r)
 
 
-##############
+################################################
 # 3) poženi model nad vsako mapo stisnjenih slik
-##############
+################################################
 
 
 for directory in os.listdir(args.img_dir):
@@ -115,7 +111,7 @@ for directory in os.listdir(args.img_dir):
     out_viz_dir = ''    # TODO popravi
     os.makedirs(out_pred_dir, exist_ok=True)
     main(out_viz_dir, out_pred_dir, args.model_type, args.model_path, img_dir, args.subimage_size, args.threshold)
-
+"""
 #################################################
 # 4) primerjaj rezultate -> zapiši v csv datoteko
 #################################################
